a759da2f879a9e71dc76bb456cc4cab250f562b5
YiBook <1275432232@qq.com>
2021-01-31 23:58:53 +0800

    feat: basic framework

---
+++ Go/Build Web Application with GoLang/article-manager/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	// Set the router as the default one provided by Gin
+	router := gin.Default()
+
+	// Process the templates at the start so that they don't have to be loaded
+	// from the disk again. This makes serving HTML pages very fast.
+	router.LoadHTMLGlob("templates/*")
+
+	// Defint the route for the index page and display the index.html template
+	// To start with, we'll use an inline route handler. Later on, we'll create
+	// standalone functions that will be used as route handlers.
+	router.GET("/", func(c *gin.Context) {
+		// Call the HTML method of the Context to render a template
+		c.HTML(
+			// Set the HTTP status to 200 (OK)
+			http.StatusOK,
+			// Use the index.html template
+			"index.html",
+			// Pass the data that the page uses (in this case, 'title')
+			gin.H{
+				"title": "Home Page",
+			},
+		)
+	})
+
+	// Start serving the application
+	router.Run()
+
+}
